16053849a2afcb14b2d10aa90b5bcf7f403d0a95
mucaca1 <matejkrc1@gmail.com>
2020-11-19 20:50:46 +0100

    - change navigation fragments

---
+++ app/src/main/java/com/example/tiktok/ui/fragments/VideoPermissionsFragment.kt
@@ -0,0 +1,38 @@
+package com.example.tiktok.ui.fragments
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.tiktok.R
+import com.example.tiktok.databinding.FragmentLoginBinding
+import com.example.tiktok.ui.viewModels.LoginViewModel
+import com.opinyour.android.app.data.utils.Injection
+
+
+class VideoPermissionsFragment : Fragment() {
+    private lateinit var loginViewModel: LoginViewModel
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_login, container, false
+        )
+        binding.lifecycleOwner = this
+        loginViewModel =
+            ViewModelProvider(this, Injection.provideViewModelFactory(requireContext()))
+                .get(LoginViewModel::class.java)
+
+        binding.model = loginViewModel
+
+
+        return binding.root
+    }
+}
