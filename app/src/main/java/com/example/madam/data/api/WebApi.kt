67a9fa0545001bea0a8d1ab577936082e325cd75
mucaca1 <matejkrc1@gmail.com>
2020-11-25 18:57:03 +0100

    WebApi
    - update

--- app/src/main/java/com/example/madam/data/api/WebApi.kt
+++ app/src/main/java/com/example/madam/data/api/WebApi.kt
@@ -2,6 +2,7 @@ package com.opinyour.android.app.data.api
 
 import com.example.madam.data.api.model.UserRegisterResponse
 import com.example.madam.data.api.model.VideoResponse
+import okhttp3.MultipartBody
 import okhttp3.OkHttpClient
 import okhttp3.RequestBody
 import retrofit2.Call
@@ -9,15 +10,59 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
 
 interface WebApi {
 
+    // Response typu boolean znamená, NIE JE IMPLEMENTOVANÉ
+
+    // Registracia
+    @POST("service.php")
+    fun register(@Body data: RequestBody): Call<UserRegisterResponse>
+
+    // Info
+    @POST("service.php")
+    fun info(@Body data: RequestBody): Call<Boolean>
+
+    // Login
+    @POST("service.php")
+    fun login(@Body data: RequestBody): Call<Boolean>
+
+    // Refresh token
+    @POST("service.php")
+    fun refreshToken(@Body data: RequestBody): Call<Boolean>
+
+    // Zmena hesla
+    @POST("service.php")
+    fun changePassword(@Body data: RequestBody): Call<Boolean>
+
+    // Pridat profilovku
+    @Multipart
+    @POST("upload.php")
+    fun uploadProfilePicture(@Part image: MultipartBody.Part, @Part("data") data: RequestBody): Call<Boolean>
+
+    // Odstanit profilovku
+    @POST("service.php")
+    fun deleteProfilePicture(@Body data: RequestBody): Call<Boolean>
+
+    // Prispevky
     @POST("upload")
     suspend fun getVideos(): Response<List<VideoResponse>>
 
+    // Pridat prispevok
+    @Multipart
+    @POST("upload.php")
+    fun addPost(@Part image: MultipartBody.Part, @Part("data") data: RequestBody): Call<Boolean>
+
+    // Existuje username
     @POST("service.php")
-    fun register(@Body data: RequestBody): Call<UserRegisterResponse>
+    fun isUsernameValid(@Body data: RequestBody): Call<Boolean>
+
+    // Vymazat prispevok
+    @POST("service.php")
+    fun deletePost(@Body data: RequestBody): Call<Boolean>
 
     companion object {
         private const val BASE_URL =
