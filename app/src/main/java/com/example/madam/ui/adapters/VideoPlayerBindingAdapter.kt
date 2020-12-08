3dfa50d51bbb4006a0aebd3df8f9284c47215b0d
michal-rohacek <michal.rohacek.256@gmail.com>
2020-12-08 21:20:17 +0100

    Commit another large mess, and hopefully final

--- app/src/main/java/com/example/madam/ui/adapters/VideoPlayerBindingAdapter.kt
+++ app/src/main/java/com/example/madam/ui/adapters/VideoPlayerBindingAdapter.kt
@@ -3,77 +3,71 @@ package com.example.madam.ui.adapters
 import android.net.Uri
 import androidx.databinding.BindingAdapter
 import com.example.madam.data.db.repositories.model.VideoItem
+import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
-import com.google.android.exoplayer2.source.MediaSourceFactory
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.ui.PlayerView
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
-import kotlinx.android.synthetic.main.video_item.view.*
 
 
 class VideoPlayerBindingAdapter {
     companion object {
 
-        val players = ArrayList<Player>()
+        private var playersMap: MutableMap<Int, SimpleExoPlayer>  = mutableMapOf()
 
-        var currentlyPlaying : Player? = null
-
-        fun registerPlayer(p: Player) {
-            players.add(p)
-        }
-
-        fun releaseRecycledPlayers(index: Int){
-            players[index].release()
-        }
-
-        fun pauseCurrentlyPlaying(){
-            if (currentlyPlaying != null){
-                currentlyPlaying?.playWhenReady = false
-                currentlyPlaying = null
+        private var currentPlayingVideo: Pair<Int, SimpleExoPlayer>? = null
+        fun pauseCurrentPlayingVideo(){
+            if (currentPlayingVideo != null){
+                currentPlayingVideo?.second?.playWhenReady = false
             }
         }
 
-        fun startPlayingByIndex(index: Int){
-            if (!players[index].playWhenReady) {
-                pauseCurrentlyPlaying()
-                players[index].playWhenReady = true
-                currentlyPlaying = players[index]
+        fun playIndexThenPausePreviousPlayer(index: Int){
+            if (playersMap.get(index)?.playWhenReady == false) {
+                pauseCurrentPlayingVideo()
+                playersMap.get(index)?.playWhenReady = true
+                currentPlayingVideo = Pair(index, playersMap.get(index)!!)
             }
-
         }
 
         fun togglePlayingState(index: Int) {
-            if (currentlyPlaying != null) pauseCurrentlyPlaying()
-            else startPlayingByIndex(index)
-        }
-
-        fun releaseAll() {
-            players.map {
-                it.release()
+            if (currentPlayingVideo != null) {
+                if (currentPlayingVideo!!.second.playWhenReady) {
+                    currentPlayingVideo!!.second.playWhenReady = false
+                } else {
+                    playIndexThenPausePreviousPlayer(index)
+                }
             }
         }
 
+        fun releaseAll() {
+            playersMap.map { it.value.release() }
+        }
+
         @JvmStatic
-        @BindingAdapter("playerObject")
-        fun PlayerView.setPlayerObject(item: VideoItem) {
-//            val trackSelector = DefaultTrackSelector(context)//
-//            trackSelector.setParameters(trackSelector.buildUponParameters().setMaxVideoSizeSd())
-//            val player = SimpleExoPlayer.Builder(context).setTrackSelector(trackSelector).build()
-//            player.setMediaItem(MediaItem.fromUri(Uri.parse(item.video_url)))
-
+        @BindingAdapter(value = ["playerObject", "item_index"])
+        fun PlayerView.setPlayerObject(item: VideoItem, item_index: Int? = null) {
             val player = SimpleExoPlayer.Builder(context).build()
-            val mediaSource = ProgressiveMediaSource.Factory(DefaultHttpDataSourceFactory("Demo")).createMediaSource(Uri.parse(item.video_url))
+            val mediaSource = ProgressiveMediaSource.Factory(DefaultHttpDataSourceFactory("Demo")).createMediaSource(
+                MediaItem.fromUri(Uri.parse(item.video_url)))
 
-            player.playWhenReady = false
+            player.playWhenReady = true
+            this.useController = false
             setKeepContentOnPlayerReset(true)
             player.repeatMode = Player.REPEAT_MODE_ALL
 
-            this.player = player
-            registerPlayer(player)
-
             player.setMediaSource(mediaSource)
             player.prepare()
+
+            this.player = player
+
+            if (playersMap.containsKey(item_index))
+                playersMap.remove(item_index)
+            if (item_index != null)
+                playersMap[item_index] = player
+
+
         }
     }
 }
